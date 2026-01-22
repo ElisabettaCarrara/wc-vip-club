b47e63e36d6fff8d9968ebdccec4eadeed3e36d6
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2026-01-22 10:56:04 +0100

    Bump plugin version to 1.1.0
    version bump

--- wc-vip-club.php
+++ wc-vip-club.php
@@ -40,7 +40,7 @@ add_action(
  *
  * @since 1.0.0
  */
-define( 'WC_VIP_CLUB_VERSION', '1.0.0' );
+define( 'WC_VIP_CLUB_VERSION', '1.1.0' );
 define( 'WC_VIP_CLUB_PLUGIN_FILE', __FILE__ );
 define( 'WC_VIP_CLUB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WC_VIP_CLUB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );

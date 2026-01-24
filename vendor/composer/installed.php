9c7eced0959018929f148cd03bab51046cf17500
elisabettac77 <146635386+elisabettac77@users.noreply.github.com>
2026-01-24 10:51:55 +0000

    chore: PHPCBF auto-fixes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'elisabettacarrara/wc-vip-club',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e3a712d2fffb24294469d1cea105012457d7abca',
+        'reference' => '4e924638de21f9966e4d8b67551ef08bbb192d7c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'elisabettacarrara/wc-vip-club' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e3a712d2fffb24294469d1cea105012457d7abca',
+            'reference' => '4e924638de21f9966e4d8b67551ef08bbb192d7c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
